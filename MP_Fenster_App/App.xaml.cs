e5f665a7c12f5c265b22fe21bd1d9079f6be69f8
Michal403 <mamcie930@gmail.com>
2026-04-29 17:03:57 +0200

    Inicjalizacja projektu WPF - szkielet aplikacji gotowy

---
+++ MP_Fenster_App/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MP_Fenster_App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
